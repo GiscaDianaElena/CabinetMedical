4802c47ca0e5d0148e7722d1f6f1db7339959946
GiscaDianaElena <101291540+GiscaDianaElena@users.noreply.github.com>
2023-03-16 17:59:15 +0200

    Update Aliment.java

--- Aliment.java
+++ Aliment.java
@@ -1,1 +1,54 @@
+public class Aliment {
+    private long dataExp;
+    private string ingrediente;
+    private double pret;
+    private double calorii;
+    private string tipAliment;
+    public Aliment(long dataExp, string ingrediente, double pret, double calorii) {
+        this.dataExp = dataExp;
+        this.ingrediente = ingrediente;
+        this.pret = pret;
+        this.calorii = calorii;
+        this.tipAliment = tipAliment;
+    }
 
+    public long getDataExp() {
+        return dataExp;
+    }
+
+    public void setDataExp(long dataExp) {
+        this.dataExp = dataExp;
+    }
+
+    public string getIngrediente() {
+        return ingrediente;
+    }
+
+    public void setIngrediente(string ingrediente) {
+        this.ingrediente = ingrediente;
+    }
+
+    public double getPret() {
+        return pret;
+    }
+
+    public void setPret(double pret) {
+        this.pret = pret;
+    }
+
+    public double getCalorii() {
+        return calorii;
+    }
+
+    public void setCalorii(double calorii) {
+        this.calorii = calorii;
+    }
+
+    public string getTipAliment() {
+        return tipAliment;
+    }
+
+    public void setTipAliment(string tipAliment) {
+        this.tipAliment = tipAliment;
+    }
+}
