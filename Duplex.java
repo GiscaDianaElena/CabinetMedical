805c62c0d706516566af7776ad25dd5e62022106
GiscaDianaElena <101291540+GiscaDianaElena@users.noreply.github.com>
2023-03-16 18:09:48 +0200

    Create Duplex.java

---
+++ Duplex.java
@@ -0,0 +1,9 @@
+public class Duplex extends Apartament{
+    public Duplex(int nrApartament, double pret, double dimensiune, string dataInchriereCumparare,
+                  string adresa, int nrCamere, boolean areSufragerie, boolean areBucatarie,
+                  int nrBai, int nrDormitoare){
+        super(9, 120.000, 130, '5.12.2021',
+                'Cosmopolis, nr. 45', 4, true, true,
+                3, 3);
+    }
+}
