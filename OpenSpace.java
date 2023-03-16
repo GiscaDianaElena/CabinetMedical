751b1756cb5b4f934787bbad55fc0b7c4d427014
GiscaDianaElena <101291540+GiscaDianaElena@users.noreply.github.com>
2023-03-16 18:10:13 +0200

    Create OpenSpace.java

---
+++ OpenSpace.java
@@ -0,0 +1,9 @@
+public class OpenSpace extends Apartament{
+    public OpenSpace(int nrApartament, double pret, double dimensiune, string dataInchriereCumparare,
+                     string adresa, int nrCamere, boolean areSufragerie, boolean areBucatarie,
+                     int nrBai, int nrDormitoare){
+        super(70, 3.000, 80.4, '15.01.2023',
+                'Crangasi, str. Morii, nr. 100', 2, true, true,
+                1);
+    }
+}
