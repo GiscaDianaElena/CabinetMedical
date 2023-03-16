94f24f044a2d5b0297b896d2067fbde4efc4d2a8
GiscaDianaElena <101291540+GiscaDianaElena@users.noreply.github.com>
2023-03-16 18:08:31 +0200

    Create Apartament2Camere.java

---
+++ Apartament2Camere.java
@@ -0,0 +1,9 @@
+public class Apartament2Camere extends Apartament{
+    public Apartament2Camere(int nrApartament, double pret, double dimensiune, string dataInchriereCumparare,
+                             string adresa, int nrCamere, boolean areSufragerie, boolean areBucatarie,
+                             int nrBai, int nrDormitoare) {
+        super(40, 50.000, 70.5, '29.08.2022',
+                'str. Splaiul Independentei nr. 45', 2, true, true,
+                1, 1);
+    }
+}
