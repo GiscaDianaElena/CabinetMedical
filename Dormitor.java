629040a3485b33b9065397bd53431cea24c2790a
GiscaDianaElena <101291540+GiscaDianaElena@users.noreply.github.com>
2023-03-16 18:12:19 +0200

    Create Dormitor.java

---
+++ Dormitor.java
@@ -0,0 +1,8 @@
+public class Dormitor extends Utilitati{
+    public Dormitor(int numarBai, int numarCamere, boolean areBalcon, boolean areAragaz, boolean areMasa,
+                    boolean areTacamuri, boolean areScaune, boolean areCanapea, boolean areTV,
+                    boolean aerConditionat, boolean arePat, boolean areDulap, boolean areNoptiere){
+        super(1, 1, true, false, false, false,
+                false, false, true, true, true, true, true);
+    }
+}
