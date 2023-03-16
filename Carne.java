6403317982d05cc89b44992779278a0146eddbcc
GiscaDianaElena <101291540+GiscaDianaElena@users.noreply.github.com>
2023-03-16 18:00:17 +0200

    Update Carne.java

--- Carne.java
+++ Carne.java
@@ -1,1 +1,5 @@
-
+public class Carne extends Aliment {
+    public Carne(long dataExp, string ingrediente, double pret, double calorii, string tipAliment) {
+        super(dataExp, ingrediente, pret, 1.05, "carne");
+    }
+}
