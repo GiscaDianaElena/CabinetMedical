32cf3fc9ebdee3513980a1731b61151101c6299b
GiscaDianaElena <101291540+GiscaDianaElena@users.noreply.github.com>
2023-03-16 18:11:24 +0200

    Create Bucatarie.java

---
+++ Bucatarie.java
@@ -0,0 +1,8 @@
+public class Bucatarie extends Utilitati{
+    public Bucatarie(int numarBai, int numarCamere, boolean areBalcon, boolean areAragaz, boolean areMasa,
+                     boolean areTacamuri, boolean areScaune,  boolean areCanapea, boolean areTV,
+                     boolean aerConditionat, boolean arePat, boolean areDulap, boolean areNoptiere){
+        super(numarBai, 1, false, true, true, true, true,
+                false, true, false, false, false, false);
+    }
+}
