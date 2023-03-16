95d1ad5086f3e00ead4031882c3ce9bd55491024
GiscaDianaElena <101291540+GiscaDianaElena@users.noreply.github.com>
2023-03-16 18:11:53 +0200

    Create Sufragerie.java

---
+++ Sufragerie.java
@@ -0,0 +1,8 @@
+public class Sufragerie extends Utilitati{
+    public Sufragerie(int numarBai, int numarCamere, boolean areBalcon, boolean areAragaz, boolean areMasa,
+                      boolean areTacamuri, boolean areScaune, boolean areCanapea, boolean areTV,
+                      boolean aerConditionat, boolean arePat, boolean areDulap, boolean areNoptiere){
+        super(numarBai, 1, true, false, false, false, true,
+                true, true, true, false, false, false);
+    }
+}
