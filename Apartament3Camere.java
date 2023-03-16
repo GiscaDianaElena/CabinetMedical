d842484e25fc5430a8e9463881e63f33599f0c5d
GiscaDianaElena <101291540+GiscaDianaElena@users.noreply.github.com>
2023-03-16 18:08:59 +0200

    Create Apartament3Camere.java

---
+++ Apartament3Camere.java
@@ -0,0 +1,9 @@
+public class Apartament3Camere extends Apartament{
+    public Apartament3Camere(int nrApartament, double pret, double dimensiune, string dataInchriereCumparare,
+                             string adresa, int nrCamere, boolean areSufragerie, boolean areBucatarie,
+                             int nrBai, int nrDormitoare){
+        super(25, 1.000, 90.2, '20.02.2023',
+                'str. Calea Plevnei, nr. 10', 3, true, true,
+                1, 2);
+    }
+}
