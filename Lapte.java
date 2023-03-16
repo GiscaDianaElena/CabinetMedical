3dd45cbac83d9e21d2f0ac835aaa78c9339f3a1f
GiscaDianaElena <101291540+GiscaDianaElena@users.noreply.github.com>
2023-03-16 18:01:14 +0200

    Update Lapte.java

--- Lapte.java
+++ Lapte.java
@@ -1,1 +1,5 @@
-
+public class Lapte extends Aliment{
+    public Lapte(long dataExp, string ingrediente, double pret, double calorii, string tipAliment){
+        super(dataExp, ingrediente, pret, 3.05, "lapte");
+    }
+}
